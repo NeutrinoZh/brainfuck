d478309768a47c4ae8ef542384888812d0cd06df
neutrinozh <neutrinozh@gmail.com>
2024-01-23 12:08:30 +0200

    program load

--- include/utils.h
+++ include/utils.h
@@ -1,1 +1,6 @@
 #include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <ios>
+#include <sstream>
+#include <vector>
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include "utils.h"
 
-int main() {
-    std::cout << "Hello, World!";
-    return 0;
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: $ brainfuck [source]" << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    std::ifstream file(argv[1], std::ios_base::binary);
+    if (!file.is_open()) {
+        std::cerr << "File " << argv[1] << " not found" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    file.seekg(0, std::ios::end);
+    std::streampos size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::vector<char> buffer(size);
+    file.read(buffer.data(), size);
+
+    file.close();
+    
+    return EXIT_SUCCESS;
 }
